745ba35037363a1ce64ffbb37da0fe22138040c0
pepek128 <33594391+pepek128@users.noreply.github.com>
2018-11-17 12:17:59 +0100

    v1

---
+++ src/main/java/com/patryk/application/BootSpringApp.java
@@ -0,0 +1,13 @@
+package com.patryk.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSpringApp.class, args);
+	}
+
+}
